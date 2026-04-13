4363d6060a338b2768f87ec46b3b3d0978d8298a
arshukla98 <arshukla@aziro.com>
2026-04-13 12:25:01 +0530

    fix

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  version         = "~> 20.0"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
@@ -13,17 +13,40 @@ module "eks" {
 
   vpc_id = module.vpc.vpc_id
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  # EKS Cluster access entries - using explicit terraform_runner entry only
+  enable_cluster_creator_admin_permissions = false
+  
+  access_entries = {
+    terraform_runner = {
+      principal_arn = data.aws_caller_identity.current.arn
+      
+      policy_associations = {
+        cluster_admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
-    instance_types         = ["t3.medium"]
+    instance_types         = ["t3.micro"]
+    disk_size              = 30
     vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
   }
 
   eks_managed_node_groups = {
 
     node_group = {
+      name         = "eks-worker-nodes"
       min_size     = 2
-      max_size     = 6
+      max_size     = 3
       desired_size = 2
     }
   }
--- vpc.tf
+++ vpc.tf
@@ -5,8 +5,10 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
+data "aws_caller_identity" "current" {}
+
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  cluster_name = "aziron-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -18,7 +20,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
-  name                 = "abhi-eks-vpc"
+  name                 = "aziron-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
